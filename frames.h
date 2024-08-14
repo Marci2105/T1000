dfc3700c3c9b57bca5544771c0463e4e6dc0f5ac
Marci2105 <133657400+Marci2105@users.noreply.github.com>
2024-08-14 08:20:20 +0200

    Update frames.h

--- frames.h
+++ frames.h
@@ -1,12 +1,8 @@
 #ifndef FRAMES_H
 #define FRAMES_H
 
-
-extern const uint8_t frame3[];
-extern const uint8_t frame4[];
-extern const uint8_t frame5[];
-extern const uint16_t frame6[];
-extern const uint16_t frame7[];
-extern const uint16_t frame8[];
+extern const uint8_t stern[];
+extern const uint8_t car[];
 
 #endif
+  
