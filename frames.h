06bf19932592af0a9c70646f959c493fcc1fb2bc
Marci2105 <133657400+Marci2105@users.noreply.github.com>
2024-08-20 11:08:53 +0200

    Update frames.h

--- frames.h
+++ frames.h
@@ -1,8 +1,18 @@
+#include <stdint.h>
 #ifndef FRAMES_H
 #define FRAMES_H
 
-extern const uint8_t stern[];
-extern const uint8_t car[];
+extern const uint8_t stern[] PROGMEM;
+
+extern const uint8_t car_close[] PROGMEM;
+
+extern const uint8_t tank[] PROGMEM;
+
+extern const uint8_t range[] PROGMEM;
+
+extern const uint8_t avg[] PROGMEM;
+
+extern const uint8_t vorneLinks[];
 
 #endif
   
