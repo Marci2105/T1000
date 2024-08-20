b00670f0c6890005618c9ce0097d7a4c26c517f7
Marci2105 <133657400+Marci2105@users.noreply.github.com>
2024-08-20 13:22:31 +0200

    Update tft_espi_lvgl.ino

--- tft_espi_lvgl.ino
+++ tft_espi_lvgl.ino
@@ -1,6 +1,6 @@
 #include <TFT_eSPI.h>
-#include <SPI.h>
 #include <lvgl.h>
+#include <iostream>
 
 #include "frames.h"
 
@@ -13,9 +13,14 @@ const int buttonPin = 32;
 int buttonState = 0;
 int page = 2;
 
-lv_obj_t *labelPageName = NULL;
 
-lv_obj_t * objectArray = NULL;
+//[0] = labelPageName
+//[1] = barChart
+//[2] = labelRange
+//[3] = containerOben
+//[4] = containerUnten
+//[5] = labelAvg
+lv_obj_t *objectArray[6] = { NULL, NULL, NULL, NULL, NULL, NULL };
 
 
 TFT_eSPI tft = TFT_eSPI(); /* TFT entity */
@@ -23,7 +28,6 @@ TFT_eSPI tft = TFT_eSPI(); /* TFT entity */
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf1[screenWidth * screenHeight / 13];
 
-//_______________________
 /* display flash */
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
   uint32_t w = (area->x2 - area->x1 + 1);
@@ -37,11 +41,16 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   lv_disp_flush_ready(disp);
 }
 
+//interrupt function von dem Button
+void IRAM_ATTR handleInterrupt() {
+  Serial.println("Interrupt occurred!");
+}
+
 void setup() {
   Serial.begin(9600); /*serial init */
 
   pinMode(buttonPin, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(buttonPin), handleInterrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), handleInterrupt, RISING);
 
   //LCD init
   tft.begin();
@@ -74,7 +83,7 @@ void setup() {
 
   drawGrayscaleBitmap(0, 0, stern, 240, 280);
 
-  stdOverlay();
+  stdOverlay(2);
 
   //overlay();
   createPageName();
@@ -99,16 +108,13 @@ void loop() {
       showPage(page);
     }
   }
+  lv_timer_handler();
 }
 
-/*interrupt function von dem Button
-void IRAM_ATTR handleInterrupt() {
-    Serial.println("Interrupt occurred!");
-} */
 
 
 // was immer angezeigt wird
-void stdOverlay() {
+void stdOverlay(int page) {
 
   lv_obj_t *containerOben = lv_obj_create(lv_scr_act());
   lv_obj_set_size(containerOben, 250, 40);
@@ -116,6 +122,7 @@ void stdOverlay() {
   lv_obj_set_style_bg_color(containerOben, lv_color_hex(0x0000ff), LV_PART_MAIN);  // Set background color to red
   lv_obj_set_style_bg_opa(containerOben, LV_OPA_COVER, LV_PART_MAIN);              // Make sure the background is fully opaque
 
+  objectArray[3] = containerOben;
   // Label Oben 1 in Container Oben
   lv_obj_t *labelO1 = lv_label_create(containerOben);
   lv_label_set_text(labelO1, "CLS 450 4 Matic");
@@ -130,6 +137,8 @@ void stdOverlay() {
   lv_obj_set_style_bg_color(containerUnten, lv_color_hex(0x909090), LV_PART_MAIN);  // Set background color to red
   lv_obj_set_style_bg_opa(containerUnten, LV_OPA_COVER, LV_PART_MAIN);              // Make sure the background is fully opaque
 
+  objectArray[4] = containerUnten;
+
   const int numPoint = 4;
   const int sizePoint = 10;
   const int sizeSpace = 15;
@@ -139,30 +148,34 @@ void stdOverlay() {
     lv_obj_set_size(point, 10, 10);
     lv_obj_align(point, LV_ALIGN_LEFT_MID, ((containerUntenBreite) / numPoint) * i, 0);
     lv_obj_set_style_radius(point, 10 / 2, LV_PART_MAIN);
-    lv_obj_set_style_bg_color(point, lv_color_white(), LV_PART_MAIN);
+    if (i + 1 == page) {
+      lv_obj_set_style_bg_color(point, lv_color_hex(0x0000ff), LV_PART_MAIN);
+    } else {
+      lv_obj_set_style_bg_color(point, lv_color_white(), LV_PART_MAIN);
+    }
     lv_obj_set_style_bg_opa(point, LV_OPA_COVER, LV_PART_MAIN);
   }
 }
 
 void createPageName() {
-  labelPageName = lv_label_create(lv_scr_act());
-  lv_obj_set_style_transform_angle(labelPageName, 900, LV_PART_MAIN);
-  lv_obj_align(labelPageName, LV_ALIGN_LEFT_MID, 20, 0);
-  lv_obj_set_style_text_color(labelPageName, lv_color_hex(0x0000ff), LV_PART_MAIN);
-  lv_label_set_text(labelPageName, "Seite 2");
+  objectArray[0] = lv_label_create(lv_scr_act());
+  lv_obj_set_style_transform_angle(objectArray[0], 900, LV_PART_MAIN);
+  lv_obj_align(objectArray[0], LV_ALIGN_LEFT_MID, 20, 0);
+  lv_obj_set_style_text_color(objectArray[0], lv_color_hex(0x0000ff), LV_PART_MAIN);
+  lv_label_set_text(objectArray[0], "Seite 2");
 }
 
 
 void pageName(const char *seitenName) {
-  lv_label_set_text(labelPageName, seitenName);
+  lv_label_set_text(objectArray[0], seitenName);
 }
 
-lv_obj_t * barChart() {
+lv_obj_t *barChart(int fuellstand) {
   lv_obj_t *bar = lv_bar_create(lv_scr_act());  // Create a bar object on the active screen
   lv_obj_set_size(bar, 150, 20);                // Set the size of the bar (width, height)
-  lv_obj_align(bar, LV_ALIGN_CENTER, 0, 0);     // Align the bar to the center of the screen
+  lv_obj_align(bar, LV_ALIGN_CENTER, 0, 80);    // Align the bar to the center of the screen
 
-  lv_bar_set_value(bar, 70, LV_ANIM_OFF);
+  lv_bar_set_value(bar, fuellstand, LV_ANIM_OFF);
   return bar;
 }
 
@@ -174,48 +187,80 @@ void clearBitmap(int sizeX, int sizeY, int posX, int posY) {
   }
 }
 
-lv_obj_t * pageOne() {
-  stdOverlay();
+void tuerenAnpassen() {
+  int vorneLinks = 0;
+  int vorneRechts = 0;
+}
+
+void pageOne() {
   int fuellstand = 20;
-  lv_obj_t * barPtr = barChart();
-  pageName("Tankfuellstand");
-  return barPtr;
+  drawGrayscaleBitmap(74, 63, tank, 114, 133);
+  objectArray[1] = barChart(fuellstand);
+  pageName("Seite 1");
 }
 
 void pageTwo() {
-  stdOverlay();
-  pageName("seite 2");
-  drawGrayscaleBitmap(75, 93, car, 89, 95);
+  clearBitmap(114, 133, 74, 63);
+  lv_obj_del(objectArray[1]);
+  objectArray[1] = NULL;
+  lv_timer_handler();
+  pageName("Seite 2");
+  drawGrayscaleBitmap(60, 40, car_close, 124, 208);
+  draw8BitRGBBitmap(100, 100, vorneLinks, 20, 53);
+
 }
 
 void pageThree() {
-  stdOverlay();
-  pageName("seite 3");
+  pageName("Seite 3");
   clearBitmap(89, 95, 75, 93);
-  // Fülle den Bildschirm mit Samuel
-  tft.setTextColor(TFT_BLACK);
-  tft.setTextSize(3);
-  tft.drawString("Seite 3", 20, 20);
-  tft.setTextSize(2);
-  tft.drawString("AbC", 20, 50);
-  tft.setTextSize(1);
-  tft.drawString("size 1", 20, 80);
+  drawGrayscaleBitmap(40, 50, range, 60, 60);
+  drawGrayscaleBitmap(40, 150, avg, 60, 60);
+
+  //style für label
+  static lv_style_t style;
+  lv_style_init(&style);
+  lv_style_set_text_font(&style, &lv_font_montserrat_20);  // Set the font to Montserrat 28
+
+  // Apply the style to the label
+  //label
+  lv_obj_t *labelRange = lv_label_create(lv_scr_act());
+  objectArray[2] = labelRange;
+  lv_obj_align(labelRange, LV_ALIGN_TOP_LEFT, 110, 70);
+  lv_obj_set_style_text_color(labelRange, lv_color_hex(0x000000), LV_PART_MAIN);
+  lv_label_set_text(labelRange, "hier Range");
+
+  lv_obj_t *labelAvg = lv_label_create(lv_scr_act());
+  objectArray[5] = labelAvg;
+  lv_obj_align(labelAvg, LV_ALIGN_TOP_LEFT, 110, 170);
+  lv_obj_set_style_text_color(labelAvg, lv_color_hex(0x000000), LV_PART_MAIN);
+  lv_label_set_text(labelAvg, "hier avg");
+
+  lv_obj_add_style(labelRange, &style, LV_PART_MAIN);
+  lv_obj_add_style(labelAvg, &style, LV_PART_MAIN);
 }
 
 void pageFour() {
-  stdOverlay();
-  pageName("seite 4");
+  pageName("Seite 4");
+  lv_obj_del(objectArray[2]);
+  lv_obj_del(objectArray[5]);
+  clearBitmap(60, 60, 40, 50);
+  clearBitmap(60, 60, 40, 150);
+
 }
 
 void showPage(int page) {
-
+  lv_obj_del(objectArray[3]);
+  objectArray[3] = NULL;
+  lv_obj_del(objectArray[4]);
+  objectArray[4] = NULL;
+  stdOverlay(page);
   if (page == 1) {
     pageOne();
   } else if (page == 2) {
     pageTwo();
   } else if (page == 3) {
     pageThree();
-  } else if (page == 4){
+  } else if (page == 4) {
     pageFour();
   }
 
@@ -240,3 +285,33 @@ uint16_t grayToRGB565(uint8_t gray) {
   uint8_t b = gray;
   return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 }
+
+void draw8BitRGBBitmap(int16_t x, int16_t y, const uint8_t *bitmap, int16_t w, int16_t h) {
+    for (int16_t j = 0; j < h; j++) {
+        for (int16_t i = 0; i < w; i++) {
+            uint8_t rgb = bitmap[j * w + i];
+            uint16_t color = rgb332ToRGB565(rgb);
+            tft.drawPixel(x + i, y + j, color);
+        }
+    }
+}
+
+uint16_t rgb332ToRGB565(uint8_t rgb332) {
+    // Extract the 3-3-2 bit components from the 8-bit RGB332 value
+    uint8_t r = (rgb332 >> 5) & 0x07;  // Red component (3 bits)
+    uint8_t g = (rgb332 >> 2) & 0x07;  // Green component (3 bits)
+    uint8_t b = rgb332 & 0x03;         // Blue component (2 bits)
+
+    // Convert 3-bit red to 5-bit red for RGB565
+    uint16_t r5 = (r * 255 / 7) >> 3;  // Scale 3-bit red to 5 bits
+    // Convert 3-bit green to 6-bit green for RGB565
+    uint16_t g6 = (g * 255 / 7) >> 2;  // Scale 3-bit green to 6 bits
+    // Convert 2-bit blue to 5-bit blue for RGB565
+    uint16_t b5 = (b * 255 / 3) >> 3;  // Scale 2-bit blue to 5 bits
+
+    // Combine the components into a 16-bit RGB565 value
+    return (r5 << 11) | (g6 << 5) | b5;
+}
+
+
+
